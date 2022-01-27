079596a3da033ad86fda3310f33f409a519aff13
Bernhard Huber <bernihuber123@gmail.com>
2022-01-27 06:47:16 +0100

    fix unit-tests

--- anttasks/src/test/java/org/huberb/prototyping/anttasks/SyncBuilderTest.java
+++ anttasks/src/test/java/org/huberb/prototyping/anttasks/SyncBuilderTest.java
@@ -18,11 +18,11 @@ package org.huberb.prototyping.anttasks;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Arrays;
 import org.apache.tools.ant.taskdefs.Sync;
 import org.apache.tools.ant.types.FileSet;
 import org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
@@ -44,34 +44,49 @@ public class SyncBuilderTest {
     public void testSync() throws IOException {
         assertNotNull(sharedTempDir);
 
-        final Path srcPath = sharedTempDir.resolve("sync-src-test-file1.ext1");
-        final File srcFile = srcPath.toFile();
-        assertFalse(srcFile.exists(), String.format("srcFile %s", srcFile.getPath()));
+        final long expectedLength = "ABC".length() * 100L;
+        for (final Path p : Arrays.asList(
+                sharedTempDir.resolve("sync-src-test-dir1/testXXX-file1-1.ext1"),
+                sharedTempDir.resolve("sync-src-test-dir1/testXXX-file2-1.ext2"),
+                sharedTempDir.resolve("sync-src-test-dir1/testXXX-file2-2.ext2"),
+                sharedTempDir.resolve("sync-src-test-dir1/testXXX-file3-1.ext3"),
+                sharedTempDir.resolve("sync-src-test-dir1/testXXX-file4-1.ext4"))) {
+            final File parentFile = p.toFile().getParentFile();
+            final File file = p.toFile();
+            if (!parentFile.exists()) {
+                assertTrue(parentFile.mkdir(), "" + parentFile);
+            }
+            assertTrue(file.createNewFile(), "" + file);
+
+            AntTasksBuilderTest.createFileContent(file, "ABC", 100);
+            assertEquals(expectedLength, file.length());
+        }
+
         final File destinationDir = sharedTempDir.resolve("sync-destination-dir-1").toFile();
         assertTrue(destinationDir.mkdir(), "" + destinationDir);
-        final File destinationFile = new File(destinationDir, "sync-src-test-file1.ext1");
-        assertFalse(destinationFile.exists(), "" + destinationFile);
 
         //---
-        assertTrue(srcFile.createNewFile());
-        final String content = "testSyncContent";
-        AntTasksBuilderTest.createFileContent(srcFile, content, 100);
-        final long expectedLength = content.length() * 100L;
-        assertEquals(expectedLength, srcFile.length());
         //---
-        final FileSet fileSet = new FileSetBuilder().dir(sharedTempDir.toString()).includes("*.ext1").build();
+        final FileSet fileSet = new FileSetBuilder()
+                .dir(sharedTempDir.toString())
+                .includes("sync-src-test-dir1/*.ext2")
+                .build();
         final AntTasksBuilder antTasksBuilder = new AntTasksBuilder();
-        final Sync copy = new SyncBuilder(antTasksBuilder.project)
+        final Sync sync = new SyncBuilder(antTasksBuilder.project)
                 .addfileset(fileSet)
                 .todir(destinationDir.getPath())
                 .build();
-        copy.execute();
+        sync.execute();
         //---
+        final File d1 = new File(destinationDir, "sync-src-test-dir1/testXXX-file2-1.ext2");
+        final File d2 = new File(destinationDir, "sync-src-test-dir1/testXXX-file2-2.ext2");
         Assertions.assertAll(
-                () -> assertTrue(srcFile.exists(), String.format("srcFile %s", srcFile.getPath())),
-                () -> assertEquals(expectedLength, srcFile.length(), String.format("srcFile %s", srcFile.getPath())),
-                () -> assertTrue(destinationFile.exists(), String.format("destinationFile %s", destinationDir)),
-                () -> assertEquals(expectedLength, destinationFile.length(), String.format("destinationFile %s", destinationDir))
+                () -> assertTrue(d1.exists(), String.format("destinationFile %s", d1)),
+                () -> assertEquals(expectedLength, d1.length(), String.format("destinationFile %s", d1))
+        );
+        Assertions.assertAll(
+                () -> assertTrue(d2.exists(), String.format("destinationFile %s", d2)),
+                () -> assertEquals(expectedLength, d2.length(), String.format("destinationFile %s", d2))
         );
     }
 
