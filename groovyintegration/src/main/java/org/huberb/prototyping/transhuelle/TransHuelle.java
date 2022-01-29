d5d170fcf41a5a13793f1b207d0a9f5f00b168c9
Bernhard Huber <bernihuber123@gmail.com>
2022-01-29 21:15:40 +0100

    algorithm calculating transitive huelle

---
+++ groovyintegration/src/main/java/org/huberb/prototyping/transhuelle/TransHuelle.java
@@ -0,0 +1,368 @@
+/*
+ * Copyright 2022 berni3.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.huberb.prototyping.transhuelle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Predicate;
+import org.huberb.prototyping.transhuelle.TransHuelle.Supports.MapBuilder;
+import org.huberb.prototyping.transhuelle.TransHuelle.Supports.SetBuilder;
+
+/**
+ *
+ * @author berni3
+ */
+public class TransHuelle {
+
+    public static void main(String[] args) {
+        final List<Data> dinList = Arrays.asList(
+                new DataFactory().createDataSample1(),
+                new DataFactory().createDataSample2(),
+                new DataFactory().createDataSample3(),
+                new DataFactory().createDataSample4()
+        );
+        for (int i = 0; i < dinList.size(); i++) {
+            final Data din = dinList.get(i);
+            System.out.println(String.format(">>>%n%s %d%n"
+                    + "din: %s%n",
+                    TransHuelle.class, i,
+                    din.toString()
+            ));
+            final Data dout = new Algorithm().evaluate(din);
+            System.out.println(String.format("%n%s %d%n"
+                    + "din: %s%n"
+                    + "dout: %s%n"
+                    + "<<<%n",
+                    TransHuelle.class, i,
+                    din.toString(), dout.toString()
+            ));
+        }
+    }
+
+    /**
+     * Wrapper for input, and out aka merged-data
+     */
+    static class Data {
+
+        static final String kName = "name";
+        static final String kGroup = "group";
+        /**
+         * name of data
+         */
+        final String name;
+        /**
+         * input list of "name": group-name, "group": group-members
+         */
+        final List<Map<String, Set<String>>> groupsInList;
+        /**
+         * merged list of "name": group-names, "group": group-members
+         */
+        final List<Map<String, Set<String>>> groupsMergedList;
+
+        /**
+         * Create empty data
+         */
+        public Data(String name) {
+            this(name, new ArrayList<>(), new ArrayList<>());
+        }
+
+        public Data(String name, List<Map<String, Set<String>>> groupsIn, List<Map<String, Set<String>>> groupsMerged) {
+            this.name = name;
+            this.groupsInList = groupsIn;
+            this.groupsMergedList = groupsMerged;
+        }
+
+        @Override
+        public String toString() {
+            final String toString = String.format("Data {%n"
+                    + "name=%s%n"
+                    + "groupsInList=%s,%n"
+                    + "groupsMergedList=%s%n"
+                    + "}",
+                    this.name,
+                    this.groupsInList, this.groupsMergedList
+            );
+            return toString;
+        }
+
+        public Map<String, Object> toMap() {
+            final Map<String, Object> result = new MapBuilder<String, Object>()
+                    .kv("name", this.name)
+                    .kv("groupsInList", this.groupsInList)
+                    .kv("groupsMergedList", this.groupsMergedList)
+                    .build();
+            return result;
+        }
+    }
+
+    /**
+     * Simple factory generating Data samples.
+     */
+    static class DataFactory {
+
+        Data createDataSample1() {
+            final Data data = new Data("dataSample1 merge+insert");
+            final List<Map<String, Set<String>>> arg0 = new ArrayList<>();
+            arg0.add(new MapBuilder<String, Set<String>>()
+                    .kv(Data.kName, SetBuilder.newSetBuilderVs("S1"))
+                    .kv(Data.kGroup, SetBuilder.newSetBuilderVs("A1", "A2"))
+                    .build());
+            arg0.add(new MapBuilder<String, Set<String>>()
+                    .kv(Data.kName, new HashSet<>(Arrays.asList("S2")))
+                    .kv(Data.kGroup, new HashSet<>(Arrays.asList("A2", "A3")))
+                    .build());
+            arg0.add(new MapBuilder<String, Set<String>>()
+                    .kv(Data.kName, new HashSet<>(Arrays.asList("S3")))
+                    .kv(Data.kGroup, new HashSet<>(Arrays.asList("A4")))
+                    .build());
+            data.groupsInList.addAll(arg0);
+            return data;
+        }
+
+        Data createDataSample2() {
+            final Data data = new Data("dataSample2 merge+insert+merge");
+            final List<Map<String, Set<String>>> arg0 = new ArrayList<>();
+            arg0.add(new MapBuilder<String, Set<String>>()
+                    .kv(Data.kName, new HashSet<>(Arrays.asList("S1")))
+                    .kv(Data.kGroup, new HashSet<>(Arrays.asList("A1", "A2")))
+                    .build());
+            arg0.add(new MapBuilder<String, Set<String>>()
+                    .kv(Data.kName, new HashSet<>(Arrays.asList("S2")))
+                    .kv(Data.kGroup, new HashSet<>(Arrays.asList("A2", "A3")))
+                    .build());
+            arg0.add(new MapBuilder<String, Set<String>>()
+                    .kv(Data.kName, new HashSet<>(Arrays.asList("S3")))
+                    .kv(Data.kGroup, new HashSet<>(Arrays.asList("A4")))
+                    .build());
+            arg0.add(new MapBuilder<String, Set<String>>()
+                    .kv(Data.kName, new HashSet<>(Arrays.asList("S4")))
+                    .kv(Data.kGroup, new HashSet<>(Arrays.asList("A1", "A4")))
+                    .build());
+            data.groupsInList.addAll(arg0);
+            return data;
+        }
+
+        Data createDataSample3() {
+            final Data data = new Data("dataSample3 no-merge-only-insert");
+            final List<Map<String, Set<String>>> arg0 = new ArrayList<>();
+            for (int i = 0; i < 10; i += 2) {
+                arg0.add(new MapBuilder<String, Set<String>>()
+                        .kv(Data.kName, new HashSet<>(Arrays.asList("S" + i)))
+                        .kv(Data.kGroup, new HashSet<>(Arrays.asList("A" + i, "A" + (i + 1))))
+                        .build());
+            }
+            data.groupsInList.addAll(arg0);
+            return data;
+        }
+
+        Data createDataSample4() {
+            final Data data = new Data("dataSample4 merge-alleven-insert-allodd");
+            final List<Map<String, Set<String>>> arg0 = new ArrayList<>();
+            for (int i = 0; i < 10; i += 1) {
+                if (i % 2 == 0) {
+                    arg0.add(new MapBuilder<String, Set<String>>()
+                            .kv(Data.kName, new HashSet<>(Arrays.asList("S" + i)))
+                            .kv(Data.kGroup, new HashSet<>(Arrays.asList("A" + i, "A" + (i + 2))))
+                            .build());
+                } else {
+                    arg0.add(new MapBuilder<String, Set<String>>()
+                            .kv(Data.kName, new HashSet<>(Arrays.asList("S" + i)))
+                            .kv(Data.kGroup, new HashSet<>(Arrays.asList("A" + i)))
+                            .build());
+                }
+            }
+            data.groupsInList.addAll(arg0);
+            return data;
+        }
+    }
+
+    /**
+     * Implementation of algorithm merging groups
+     */
+    static class Algorithm {
+
+/**
+
+@param in 
+@return 
+*/
+        Data evaluate(final Data in) {
+            final long startTime = System.currentTimeMillis();
+            final Data out = new Data(in.name);
+            out.groupsInList.addAll(in.groupsInList);
+
+            //---
+            for (final Map<String, Set<String>> groupsInElement : out.groupsInList) {
+                final String ingname = groupsInElement.get(Data.kName).iterator().next();
+                final Set<String> ingmembers = groupsInElement.get(Data.kGroup);
+                //---
+                final List<Map<String, Object>> opList = new ArrayList<>();
+                if (out.groupsMergedList.isEmpty()) {
+                    // add in to out
+                    final Map<String, Object> opInsert = new MapBuilder<String, Object>()
+                            .kv("op", "insert")
+                            .kv("ingname", ingname)
+                            .kv("ingmembers", ingmembers)
+                            .build();
+                    opList.add(opInsert);
+                } else {
+                    int matchCount = 0;
+                    for (final Map<String, Set<String>> groupsMergedElement : out.groupsMergedList) {
+                        final Set<String> outnames = groupsMergedElement.get(Data.kName);
+                        final Set<String> outgmembers = groupsMergedElement.get(Data.kGroup);
+
+                        final boolean match = ingmembers.stream().anyMatch((ingmember) -> outgmembers.contains(ingmember));
+                        if (match) {
+                            if (matchCount == 0) {
+                                final Map<String, Object> opMerge = new MapBuilder<String, Object>()
+                                        .kv("op", "merge")
+                                        .kv("ingname", ingname)
+                                        .kv("ingmembers", ingmembers)
+                                        .kv("outnames", outnames)
+                                        .kv("outgmembers", outgmembers)
+                                        .build();
+                                opList.add(opMerge);
+                            } else {
+                                final Map<String, Object> opDelete = new MapBuilder<String, Object>()
+                                        .kv("op", "delete")
+                                        .kv("ingname", ingname)
+                                        .kv("ingmembers", ingmembers)
+                                        .kv("outnames", outnames)
+                                        .kv("outgmembers", outgmembers)
+                                        .build();
+                                opList.add(opDelete);
+                            }
+                            matchCount += 1;
+                        }
+                    }
+                    // PostProcess matchCount=0
+                    if (matchCount == 0) {
+                        final Map<String, Object> opInsert = new MapBuilder<String, Object>()
+                                .kv("op", "insert")
+                                .kv("ingname", ingname)
+                                .kv("ingmembers", ingmembers)
+                                .build();
+                        opList.add(opInsert);
+                    }
+                }
+                // Process opList
+                System.out.println(String.format("Process opList: %s", opList));
+                for (final Map<String, Object> opElement : opList) {
+
+                    final String op = (String) opElement.getOrDefault("op", "-");
+                    if ("merge".equals(op)) {
+                        // can merge
+                        final String op_ingname = (String) opElement.get("ingname");
+                        final Set<String> op_ingmembers = (Set<String>) opElement.get("ingmembers");
+                        final Set<String> op_outnames = (Set<String>) opElement.get("outnames");
+                        final Set<String> op_outgmembers = (Set<String>) opElement.get("outgmembers");
+
+                        op_outnames.add(op_ingname);
+                        op_outgmembers.addAll(op_ingmembers);
+                    } else if ("insert".equals(op)) {
+                        // add in to out
+                        final String op_ingname = (String) opElement.get("ingname");
+                        final Set<String> op_ingmembers = (Set<String>) opElement.get("ingmembers");
+
+                        out.groupsMergedList.add(new MapBuilder<String, Set<String>>()
+                                .kv(Data.kName, new HashSet<String>(Arrays.asList(op_ingname)))
+                                .kv(Data.kGroup, new HashSet<>(op_ingmembers))
+                                .build()
+                        );
+                    } else if ("delete".equals(op)) {
+                        // delete matched
+                        final String op_ingname = (String) opElement.get("ingname");
+                        final Set<String> op_ingmembers = (Set<String>) opElement.get("ingmembers");
+                        final Set<String> op_outnames = (Set<String>) opElement.get("outnames");
+                        final Set<String> op_outgmembers = (Set<String>) opElement.get("outgmembers");
+                        final Predicate<Map<String, Set<String>>> p = (m
+                                -> m.get(Data.kName) == op_outnames
+                                && m.get(Data.kGroup) == op_outgmembers);
+                        out.groupsMergedList.removeIf(p);
+                    }
+                }
+            }
+            final long endTime = System.currentTimeMillis();
+            System.out.println(String.format("evaluate start %d ms, end %d ms, duration %d ms", startTime, endTime, (endTime - startTime)));
+            return out;
+        }
+    }
+
+    /**
+     * Supporting classes
+     */
+    static class Supports {
+
+        /**
+         * Supports building a Map
+         */
+        static class MapBuilder<K, V> {
+
+            final Map<K, V> m = new LinkedHashMap<>();
+
+            MapBuilder<K, V> kv(K k, V v) {
+                this.m.put(k, v);
+                return this;
+            }
+
+            Map<K, V> build() {
+                return this.m;
+            }
+
+        }
+
+        static class SetBuilder<V> {
+
+            static Set<String> newSetBuilderVs(String... vs) {
+                final Set<String> result = new SetBuilder<String>()
+                        .vs(vs)
+                        .build();
+                return result;
+            }
+
+            final Set<V> s = new LinkedHashSet<>();
+
+            SetBuilder<V> v(Collection<V> vc) {
+                this.s.addAll(vc);
+                return this;
+            }
+
+            SetBuilder<V> v(V v) {
+                this.s.add(v);
+                return this;
+            }
+
+            SetBuilder<V> vs(V... vs) {
+                for (int i = 0; i < vs.length; i++) {
+                    this.s.add(vs[i]);
+                }
+                return this;
+            }
+
+            Set<V> build() {
+                return this.s;
+            }
+        }
+
+    }
+}
---
+++ groovyintegration/src/test/java/org/huberb/prototyping/transhuelle/AlgorithmTest.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2022 berni3.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.huberb.prototyping.transhuelle;
+
+import groovy.json.JsonGenerator;
+import groovy.json.JsonGenerator.Options;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.huberb.prototyping.transhuelle.TransHuelle.Algorithm;
+import org.huberb.prototyping.transhuelle.TransHuelle.Data;
+import org.huberb.prototyping.transhuelle.TransHuelle.DataFactory;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.yaml.snakeyaml.Yaml;
+
+/**
+ *
+ * @author berni3
+ */
+public class AlgorithmTest {
+
+    public AlgorithmTest() {
+    }
+
+    /**
+     * Test of evaluate, of class Algorithm.
+     */
+    @Test
+    public void given_createDataSample1_verify_groupsMergedList() {
+        final Data din = new DataFactory().createDataSample1();
+        final Data dout = new Algorithm().evaluate(din);
+
+        assertEquals(3, dout.groupsInList.size(), "" + dout);
+        assertEquals(2, dout.groupsMergedList.size(), "" + dout);
+        assertTrue(dout.groupsMergedList.get(0).get(Data.kName).containsAll(Arrays.asList("S1", "S2")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(0).get(Data.kGroup).containsAll(Arrays.asList("A1", "A2", "A3")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(1).get(Data.kName).containsAll(Arrays.asList("S3")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(1).get(Data.kGroup).containsAll(Arrays.asList("A4")), "" + dout);
+    }
+
+    @Test
+    public void given_createDataSample2_verify_groupsMergedList() {
+        final Data din = new DataFactory().createDataSample2();
+        final Data dout = new Algorithm().evaluate(din);
+
+        assertEquals(4, dout.groupsInList.size(), "" + dout);
+        assertEquals(1, dout.groupsMergedList.size(), "" + dout);
+        assertTrue(dout.groupsMergedList.get(0).get(Data.kName).containsAll(Arrays.asList("S1", "S2")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(0).get(Data.kGroup).containsAll(Arrays.asList("A1", "A2", "A3", "A4")), "" + dout);
+    }
+    @Test
+    public void given_createDataSample3_verify_groupsMergedList() {
+        final Data din = new DataFactory().createDataSample3();
+        final Data dout = new Algorithm().evaluate(din);
+
+assertEquals("dataSample3 no-merge-only-insert", dout.name);
+        assertEquals(5, dout.groupsInList.size(), "" + dout);
+        assertEquals(5, dout.groupsMergedList.size(), "" + dout);
+        assertTrue(dout.groupsMergedList.get(0).get(Data.kName).containsAll(Arrays.asList("S0")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(0).get(Data.kGroup).containsAll(Arrays.asList("A0", "A1")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(1).get(Data.kName).containsAll(Arrays.asList("S2")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(1).get(Data.kGroup).containsAll(Arrays.asList("A2", "A3")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(2).get(Data.kName).containsAll(Arrays.asList("S4")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(2).get(Data.kGroup).containsAll(Arrays.asList("A4", "A5")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(3).get(Data.kName).containsAll(Arrays.asList("S6")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(3).get(Data.kGroup).containsAll(Arrays.asList("A6", "A7")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(4).get(Data.kName).containsAll(Arrays.asList("S8")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(4).get(Data.kGroup).containsAll(Arrays.asList("A8", "A9")), "" + dout);
+    }
+    @Test
+    public void given_createDataSample4_verify_groupsMergedList() {
+        final Data din = new DataFactory().createDataSample4();
+        final Data dout = new Algorithm().evaluate(din);
+
+assertEquals("dataSample4 merge-alleven-insert-allodd", dout.name);
+        assertEquals(10, dout.groupsInList.size(), "" + dout);
+        assertEquals(6, dout.groupsMergedList.size(), "" + dout);
+        assertTrue(dout.groupsMergedList.get(0).get(Data.kName).containsAll(Arrays.asList("S0", "S2", "S4", "S6", "S8")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(0).get(Data.kGroup).containsAll(Arrays.asList("A0", "A2", "A4", "A6", "A8")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(1).get(Data.kName).containsAll(Arrays.asList("S1")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(1).get(Data.kGroup).containsAll(Arrays.asList("A1")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(2).get(Data.kName).containsAll(Arrays.asList("S3")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(2).get(Data.kGroup).containsAll(Arrays.asList("A3")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(3).get(Data.kName).containsAll(Arrays.asList("S5")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(3).get(Data.kGroup).containsAll(Arrays.asList("A5")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(4).get(Data.kName).containsAll(Arrays.asList("S7")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(4).get(Data.kGroup).containsAll(Arrays.asList("A7")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(5).get(Data.kName).containsAll(Arrays.asList("S9")), "" + dout);
+        assertTrue(dout.groupsMergedList.get(5).get(Data.kGroup).containsAll(Arrays.asList("A9")), "" + dout);
+    }
+
+    static class Generators {
+
+        void toYaml(Data dout) {
+            final Yaml yaml = new Yaml();
+            final String doutYaml = yaml.dump(dout.toMap());
+            System.out.println(String.format("doutYaml:%n%s%n", doutYaml));
+        }
+
+        void toJson(Data dout) {
+            JsonGenerator jsonGenerator = new Options().build();
+            final String doutJson = jsonGenerator.toJson(dout.toMap());
+            System.out.println(String.format("doutJson:%n%s%n", doutJson));
+        }
+
+        void toCsv(Data dout) {
+            final String doutCsv = new CsvGenerator().toCsv(dout.toMap());
+            System.out.println(String.format("doutCsv:%n%s%n", doutCsv));
+        }
+
+        static class CsvGenerator {
+
+            String toCsv(Map<String, Object> m) {
+                //List<String> keys = m.keySet().stream().sorted().collect(Collectors.toList());
+                List<Map.Entry<String, Object>> mapEntryList = m.entrySet().stream()
+                        .sorted(Map.Entry.comparingByKey())
+                        .collect(Collectors.toList());
+                final StringBuilder csvLines = new StringBuilder();
+                for (Map.Entry<String, Object> mapEntry : mapEntryList) {
+                    String[] lineValues = new String[]{mapEntry.getKey(), mapEntry.getValue().toString()};
+                    String csvLine = Stream.of(lineValues)
+                            .map(this::escapeSpecialCharacters)
+                            .collect(Collectors.joining(","));
+                    csvLines.append(csvLine).append("\n");
+
+                }
+                return csvLines.toString();
+            }
+
+            public String escapeSpecialCharacters(String data) {
+                String escapedData = data.replaceAll("\\R", " ");
+                if (data.contains(",") || data.contains("\"") || data.contains("'")) {
+                    data = data.replace("\"", "\"\"");
+                    escapedData = "\"" + data + "\"";
+                }
+                return escapedData;
+            }
+        }
+    }
+}
