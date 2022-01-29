63c2eac3b3901b4e868162d9e61916b79ffb517f
Bernhard Huber <bernihuber123@gmail.com>
2022-01-29 18:28:57 +0100

    start groovyshellscript integration

---
+++ groovyintegration/src/main/java/org/huberb/prototyping/groovyintegration/GshloaderMain.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2022 berni3.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.huberb.prototyping.groovyintegration;
+
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+import java.io.File;
+import java.util.Date;
+import java.util.concurrent.Callable;
+import java.util.logging.Logger;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+/**
+ *
+ * @author berni3
+ */
+@Command(name = "gshloader",
+        mixinStandardHelpOptions = true,
+        showAtFileInUsageHelp = true,
+        showDefaultValues = true,
+        version = "gshloader 1.0-SNAPSHOT",
+        description = "Load a groovyscript file")
+public class GshloaderMain implements Callable<Integer> {
+
+    private static final Logger logger = Logger.getLogger(GshloaderMain.class.getName());
+
+    @Option(names = {"-g", "--groovyscript-file"},
+            required = false,
+            description = "load groovy script file")
+    private File groovyScriptFile;
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new GshloaderMain()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+        logger.info(() -> String.format("Process groovy-script-file %s", groovyScriptFile));
+
+        evaluateGroovyScriptFile(this.groovyScriptFile);
+        return 0;
+    }
+
+    void evaluateGroovyScriptFile(File groovyScriptFile) {
+        var sharedData = new Binding();
+        var shell = new GroovyShell(sharedData);
+        var now = new Date();
+        sharedData.setProperty("text", "I am shared data!");
+        sharedData.setProperty("date", now);
+
+        final Object result = shell.evaluate("\"At $date, $text\"");
+
+        logger.info(() -> String.format(" evaluate result `%s´", result));
+        //assert result == "At $now, I am shared data!"
+    }
+
+}
